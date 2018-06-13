7b19a798d8469dc594e6267134f2787c1ebca94a
Isaac Villicana <villicana.i@gmail.com>
2018-06-13 17:43:00 -0400

    add scraper method

--- lib/billboard_chart/song.rb
+++ lib/billboard_chart/song.rb
@@ -1,6 +1,6 @@
 class BillboardChart::Song
     # set up attributes for each instance
-    attr_accessor :title, :artist, :current_rank, :last_week_rank
+    attr_accessor :title, :artist, :current_rank, :last_week_rank, :peak_position, :weeks
 
     @@all = []
 
@@ -11,28 +11,36 @@ class BillboardChart::Song
 
     # Return this week's Billboard Hot 100 based on scraped data
     def self.this_week
-        
+        @@all
     end
 
     # Scrape songs from https://www.billboard.com/charts/hot-100
     def self.scrape_songs
-        # title: rows.css(".chart-row__song").text
-        # artist: rows.css(".chart-row__artist").text
-        # current_rank: rows.css(".chart-row__current-week").text
-        # last_week_rank: rows.css(".chart-row__last-week").text
-        # peak_position:
-        # weeks:
+        # title: row.css(".chart-row__song").text
+        # artist: row.css(".chart-row__artist").text.strip
+        # current_rank: row.css(".chart-row__current-week").text
+        # last_week_rank: row.css(".chart-row__secondary .chart-row__last-week .chart-row__value").text
+        # peak_position: row.css(".chart-row__secondary .chart-row__top-spot .chart-row__value").text
+        # weeks: row.css(".chart-row__secondary .chart-row__weeks-on-chart .chart-row__value").text
         
         page_html = Nokogiri::HTML(open("https://www.billboard.com/charts/hot-100"))
         chart = page_html.css(".chart-data .container")
-        rows = chart.css(".chart-row")
         
-        rows.each do |row|
+        #Iterate through the first 10 rows of the billboard chart
+        #Create instances of the Song class during each iteration
+        i = 1
+        10.times do 
+            row = chart.css(".chart-row--#{i}")
             song = BillboardChart::Song.new
-            song.name = row.css(".chart-row__song").text
+            song.title = row.css(".chart-row__song").text
+            song.artist = row.css(".chart-row__artist").text.strip
+            song.current_rank = row.css(".chart-row__current-week").text
+            song.last_week_rank = row.css(".chart-row__secondary .chart-row__last-week .chart-row__value").text
+            song.peak_position = row.css(".chart-row__secondary .chart-row__top-spot .chart-row__value").text
+            song.weeks = row.css(".chart-row__secondary .chart-row__weeks-on-chart .chart-row__value").text
+            i += 1
         end
-
-        binding.pry
-        # Create instances of BillboardChart::Song class
+        @@all
+        
     end
 end
