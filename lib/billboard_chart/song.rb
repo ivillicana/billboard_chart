5d1496ab51c0dea4f86255c32ec67e5833b01bb6
Isaac Villicana <villicana.i@gmail.com>
2018-06-13 17:06:00 -0400

    add nokogiri code

--- lib/billboard_chart/song.rb
+++ lib/billboard_chart/song.rb
@@ -1,5 +1,13 @@
 class BillboardChart::Song
     # set up attributes for each instance
+    attr_accessor :title, :artist, :current_rank, :last_week_rank
+
+    @@all = []
+
+    # initialize song object and add to @@all array
+    def initialize
+        @@all << self
+    end
 
     # Return this week's Billboard Hot 100 based on scraped data
     def self.this_week
@@ -8,13 +16,23 @@ class BillboardChart::Song
 
     # Scrape songs from https://www.billboard.com/charts/hot-100
     def self.scrape_songs
-        # title:
-        # artist:
-        # last_week:
+        # title: rows.css(".chart-row__song").text
+        # artist: rows.css(".chart-row__artist").text
+        # current_rank: rows.css(".chart-row__current-week").text
+        # last_week_rank: rows.css(".chart-row__last-week").text
         # peak_position:
         # weeks:
+        
+        page_html = Nokogiri::HTML(open("https://www.billboard.com/charts/hot-100"))
+        chart = page_html.css(".chart-data .container")
+        rows = chart.css(".chart-row")
+        
+        rows.each do |row|
+            song = BillboardChart::Song.new
+            song.name = row.css(".chart-row__song").text
+        end
 
-
+        binding.pry
         # Create instances of BillboardChart::Song class
     end
 end
