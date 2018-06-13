4e3d829d0b8ee1bfe6a7dfec897482693a902f53
Isaac Villicana <villicana.i@gmail.com>
2018-06-13 17:58:11 -0400

    cli prints top 10 songs

--- lib/billboard_chart/cli.rb
+++ lib/billboard_chart/cli.rb
@@ -8,6 +8,15 @@ class BillboardChart::CLI
     # list songs from scraper
     def list_songs
         puts "Here are this week's Billboard Hot 10 songs:"
+        top_songs
+    end
+
+    def top_songs
+        #prints out the top 10 songs in chart for the week
+        @songs = BillboardChart::Song.this_week
+        @songs.each.with_index(1) do |song, i|
+            puts "#{i}. #{song.title} - #{song.artist}"
+        end
     end
 
     # displays main menu
