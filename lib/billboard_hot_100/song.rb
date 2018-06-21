ba365004b3206c86761de5bbc1d5e90a4d9bdc28
Isaac Villicana <villicana.i@gmail.com>
2018-06-21 16:12:15 -0400

    delete #scrape_songs

--- lib/billboard_hot_100/song.rb
+++ lib/billboard_hot_100/song.rb
@@ -1,31 +1,22 @@
 class BillboardHot100::Song
+    # Build a Scraper class that scrapes data and sends it to Song to create new song instances
+
     # set up attributes for each instance
     attr_accessor :title, :artist, :current_rank, :last_week_rank, :peak_position, :weeks
+    @@all = []
 
     # Return this week's Billboard Hot 100 based on scraped data
-    def self.this_week(range)
-        @@all ||= self.scrape_songs
-        @@all["#{range-1}0".to_i, 10] #selects only 10 songs within selected range
+    def initialize
+        @@all << self
     end
 
-    # Scrape songs from https://www.billboard.com/charts/hot-100
-    def self.scrape_songs
-        page_html = Nokogiri::HTML(open("https://www.billboard.com/charts/hot-100"))
-        chart = page_html.css(".chart-data .container")
-        
-        #Iterate through the first 100 rows of the billboard chart
-        #Create instances of the Song class during each iteration
-        rows = chart.css(".chart-row")
-        rows.collect do |row|
-            song = self.new
-            song.title = row.css(".chart-row__song").text
-            song.artist = row.css(".chart-row__artist").text.strip
-            song.current_rank = row.css(".chart-row__current-week").text
-            song.last_week_rank = row.css(".chart-row__secondary .chart-row__last-week .chart-row__value").text
-            song.peak_position = row.css(".chart-row__secondary .chart-row__top-spot .chart-row__value").text
-            song.weeks = row.css(".chart-row__secondary .chart-row__weeks-on-chart .chart-row__value").text
-            song
-        end  
+    # build a class getter method for @@all
+    def self.all
+        @@all
+    end
+
+    def self.this_week(range)
+        @@all["#{range-1}0".to_i, 10] #selects only 10 songs within selected range
     end
 
     # Print info for chosen song
